1445d31613ecbe5ebeebd23426301f50c2c05198
David O'Dey <david.o'dey@pearson.com>
2016-11-30 07:33:44 -0700

    loaded all files into react

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import './main/assets/css/style.css';
+import './main/assets/css/pattern-scaffolding.css';
+import './main/assets/css/iCheck.1.0.2/all.css';
+;
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
