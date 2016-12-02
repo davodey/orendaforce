48c49e0ac2958aa59d5f6fcad27cc438b1e6e659
David O'Dey <david.o'dey@pearson.com>
2016-12-02 09:10:37 -0700

    bug - fixed issue in safari where the reviews were not being generated

--- src/yelp/components/Review.js
+++ src/yelp/components/Review.js
@@ -1,8 +1,11 @@
 import React from "react";
+import dateFormat from 'dateformat';
 
 const YelpList = (props) => {
     let image;
-
+    let reviewDate = props.date;
+    reviewDate = reviewDate.substring(0, reviewDate.indexOf(' '));
+    const newDate = dateFormat(reviewDate, "mmmm dS, yyyy");
     if (props.picture === null)
         image = "../../images/no-profile-pic.png";
      else
@@ -16,7 +19,7 @@ const YelpList = (props) => {
                 </div>
                 <div className="yelper-info">
                     <h3>{props.username}</h3>
-                    <h4>{props.date}</h4>
+                    <h4>{ newDate }</h4>
                 </div>
                 <div className="yelper-rating">
                     <div className="rating" data-rating={props.rating}>
