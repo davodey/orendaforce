bb6f041347bee3a5275e5a276e03d892ce5bb359
David O'Dey <david.o'dey@pearson.com>
2016-12-02 15:28:26 -0700

    made some minor revisions for mobile

--- _tasks/compile-scss.js
+++ _tasks/compile-scss.js
@@ -10,5 +10,6 @@ gulp.task('compile-scss', function () {
         })
         .pipe(autoprefixer('last 2 version', 'safari 4', 'ie 8', 'ie 9', 'ie 10', 'opera 12.1', 'ios 6', 'android 4','Firefox > 16'))
         .pipe(gulp.dest('pattern-lab/source/css'))
+        .pipe(gulp.dest('../app/src/main/assets/css'))
         .pipe(gulp.dest('ui/css'));
 });
