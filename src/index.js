e52f6baf3f51391a2df9704e8db0afc9e4f09158
David O'Dey <david.odey@pearson.com>
2017-03-06 15:00:41 -0700

    added www to domain name

--- src/index.js
+++ src/index.js
@@ -19,14 +19,14 @@ class YelpContainer extends Component {
     }
 
     componentDidMount() {
-        fetch("http://orendaforce.com/api")
+        fetch("http://www.orendaforce.com/api")
             .then( (response) => {
                 return response.json() })
             .then( (json) => {
                 this.setState({reviews: json});
             });
 
-        fetch("http://orendaforce.com/api/biz")
+        fetch("http://www.orendaforce.com/api/biz")
             .then( (response) => {
                 return response.json() })
             .then( (json) => {
